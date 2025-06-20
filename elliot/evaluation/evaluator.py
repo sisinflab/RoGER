668d302b2798cc153d962ebf06ae5f1df5488a4f
NotaDomi <notadomi@gmail.com>
2025-06-20 11:05:20 +0200

    fix eval metric visualization

--- elliot/evaluation/evaluator.py
+++ elliot/evaluation/evaluator.py
@@ -107,7 +107,7 @@ class Evaluator(object):
         self.logger.info(f"MSE: {val_results:.16f}")
         self.logger.info(f"Test Evaluation results")
         self.logger.info(f"Results")
-        self.logger.info(f"MSE: {test_results:.4f}")
+        self.logger.info(f"MSE: {test_results:.16f}")
 
         result_dict = {0: {"val_results": {'MSE': val_results},
                            "val_statistical_results": [],
@@ -126,10 +126,10 @@ class Evaluator(object):
         self.logger.info("")
         self.logger.info(f"Validation Evaluation results")
         self.logger.info(f"Results")
-        self.logger.info(f"AUC: {val_results:.4f}")
+        self.logger.info(f"AUC: {val_results:.16f}")
         self.logger.info(f"Test Evaluation results")
         self.logger.info(f"Results")
-        self.logger.info(f"AUC: {test_results:.4f}")
+        self.logger.info(f"AUC: {test_results:.16f}")
 
         result_dict = {0: {"val_results": {'AUC': val_results},
                            "val_statistical_results": [],
